8af0a1aa516c094101dad68793b5927ae9b618eb
astelvida <sevda.m.anefi@gmail.com>
2016-12-21 20:21:16 -0800

    configured routing react

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {render} from 'react-dom';
 import AwesomeComponent from './components/AwesomeComp.jsx';
+import {Router, Route, browserHistory, hashHistory, IndexRoute} from "react-router";
 
 
 class App extends React.Component {
   render () {
     return (
     	<div>
-    		<p> yooooooo!</p>
-    		<AwesomeComponent />
-    	</div>
+    		<p> wtf </p>
+    		// <AwesomeComponent />
+	  	  <Router history = {browserHistory}>
+			    <Route path='/' component= {Home} />
+			    <Route path='/bigdogs' component={AwesomeComponent} />
+			  </Router>
+		  </div>
     )
   }
 }
+
+const Home = () => <h1>Hello from Home!</h1>
+
 
 render(<App/>, document.getElementById('app'));
