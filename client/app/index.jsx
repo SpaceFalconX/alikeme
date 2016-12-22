c6c16aa1fd6dc303184d5910e7e3746754fd3a4e
Isaac <Isaac@Isaacs-MacBook-Pro.local>
2016-12-21 21:54:10 -0800

    routing

--- client/app/index.jsx
+++ client/app/index.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
-import {Router, Route, browserHistory, IndexRoute} from "react-router";
+import {Router, Route, browserHistory, hashHistory, IndexRoute} from "react-router";
 import AwesomeComponent from './components/AwesomeComp.jsx';
 
 
 class App extends React.Component {
   render () {
     return (
-  <Router history = {browserHistory}>
-    <Route path={"/"}component={AwesomeComponent}>
-      <Route path={'/bigdogs'} component={AwesomeComponent} />
-    </Route>
+  <Router history = {hashHistory}>
+    <Route path={'/bigdogs'} component={AwesomeComponent} />
   </Router>
     )
   }
