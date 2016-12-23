b56830dbcd02e8aae6cef703a698fd8d1ce17b87
astelvida <sevda.m.anefi@gmail.com>
2016-12-23 11:36:47 -0800

    hash passwords with bcrypt

--- server/database/config.js
+++ server/database/config.js
@@ -1,4 +1,6 @@
-const Sequelize = require('sequelize')
+const bcrypt = require('bcryptjs');
+const Sequelize = require('sequelize');
+const _ = require('underscore');
 const dbConnection = new Sequelize('alikeMe','root','123');
 
 const User = dbConnection.define('user', {
@@ -29,21 +31,54 @@ const User = dbConnection.define('user', {
   		isEmail: true
   	}
   }
+}, {
+	hooks: {
+		beforeBulkCreate: function (users) {
+			_.each(users, (user) => (user.password = bcrypt.hashSync(user.password, 8)))
+		}
+	}
 })
+
+
 
 dbConnection.sync({
 	force: true,
 	logging: console.log
 })
 .then(() => (
-	User.create({
-		username: "clara",
-		email: "clara@gmail.com",
-		password: 1
+	User.bulkCreate([{
+		username: "wasiff",
+		email: "wasiff@gmail.com",
+		password: "1"
+	}, {
+		username: "isaac",
+		email: "isaac@gmail.com",
+		password: "1"
+	}, {
+		username: "sevda",
+		email: "sevda@gmail.com",
+		password: "1"
+	}], {
+		validate: true,
+		ignoreDuplicates: true
 	})
+	.then((users) => (
+		_.each(users, (user)=>( console.log(user.dataValues) ))
+	))
 ))
 .catch((error) => (
 	console.log(error)
 ))
 
 module.exports = dbConnection;
+
+
+
+
+
+
+
+
+
+
+
