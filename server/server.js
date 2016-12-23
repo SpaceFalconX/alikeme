9d727c632acf47614bc744ff7d88f255ee6561cf
astelvida <sevda.m.anefi@gmail.com>
2016-12-23 11:36:47 -0800

    hash passwords with bcrypt

--- server/server.js
+++ server/server.js
@@ -1,13 +1,19 @@
-var express = require('express');
-var path = require('path')
-var app = express();
-var db = require('./database/config.js');
+const express = require('express');
+const path = require('path')
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const db = require('./database/config.js');
+const app = express();
 
+// MIDDLEWARE FUNCTIONS
 app.use(express.static('client'));
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
 app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, '../client', 'index.html'))
 })
+
 
 app.listen(4000, function() {
 	console.log("app running on port 4000")
