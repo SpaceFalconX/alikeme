fca6e4b03390a1a91dd7dac0d6ecc83e08ac1094
astelvida <sevda.m.anefi@gmail.com>
2016-12-23 12:02:32 -0800

    pull req...

--- server/server.js
+++ server/server.js
@@ -14,7 +14,6 @@ app.get('*', function (request, response){
   response.sendFile(path.resolve(__dirname, '../client', 'index.html'))
 })
 
-
 app.listen(4000, function() {
 	console.log("app running on port 4000")
 })
