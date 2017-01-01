a2e6235478fc473cc644df29a62c5807df419bbb
Isaac <Isaac@Isaacs-MacBook-Pro.local>
2016-12-31 16:10:24 -0800

    applied rebase

--- client/app/reducers/index.js
+++ client/app/reducers/index.js
@@ -1,22 +1,14 @@
 import {combineReducers} from 'redux';
 import {auth} from './auth.js'
-<<<<<<< 74096448a803b77a3b969ec577ddfdffd3b82730
 import {userPosts} from './post.js'
-=======
 import {tags} from './tags.js'
->>>>>>> rebase
 import {routerReducer} from 'react-router-redux';
 
 const reducer = combineReducers({
 	user: auth,
-<<<<<<< 74096448a803b77a3b969ec577ddfdffd3b82730
 	userPosts: userPosts,
-	routing: routerReducer
-})
-=======
 	routing: routerReducer,
   tags
-}) //make sure name matches in props
->>>>>>> rebase
+})
 
 export default reducer;
