8f5cdde1d01319bc098ba516be9f7d333c5289d0
astelvida <sevda.m.anefi@gmail.com>
2017-01-04 10:58:39 -0800

    created_at and updated_at to post get

--- server/routes/post_router.js
+++ server/routes/post_router.js
@@ -1,11 +1,11 @@
-
 const Post = require('../database/models/post.js')
 const Posts = require('../database/collections/posts.js')
 const Tag = require('../database/models/tag.js')
 const Tags = require('../database/collections/tags.js')
 
 const Promise = require('bluebird')
-const _ = require('lodash')
+const _ = require('lodash');
+const __ = require('underscore');
 const express = require('express');
 
 const router = express.Router();
@@ -20,28 +20,37 @@ router.route('/posts')
 			.then((allTags) => {
 				const allTagsNames = allTags.pluck('name');
 				const newTagsNames = JSON.parse(tags)
-				const tagsToSave = _.difference(newTagsNames, allTagsNames);
-				const promisedTags = tagsToSave.map((tagName, index) => {
+				const promisedTags = _.difference(newTagsNames, allTagsNames)
+				.map((tagName, index) => {
 					return Tag.forge({name: tagName}).save();
 				})
 				return Promise.all(promisedTags)
 				.then((results) => {
 					post.tags().attach(results);
-					console.log("TAGS, POSTS and POSTS_TAGS have been updated")
-					res.json({data: "SUCCESS: created post and added tags" })
+					res.sendStatus(201);
 				})
 			})
 		})
 	})
-	
+
+
 	.get((req, res) => {
 		Posts.forge()
-		.fetch({withRelated: ['tags', 'category']})
-		.then((data) => {
-			res.json({data})
+		.fetch({withRelated: ['user', 'category', 'tags']})
+		.then((collection) => {
+			let result = collection.toJSON();
+			for(let i = 0; i < result.length; i++) {
+				result[i] = _.pick(result[i], ['title', 'created_at', 'updated_at', 'content', 'id', 
+																			 'user.username', 'user.id', 'category.id', 'category.name', 'tags'])
+				for(let j = 0; j < result[i].tags.length; j++) {
+					delete result[i].tags[j]['_pivot_id'];
+					delete result[i].tags[j]['_pivot_post_id'];
+					delete result[i].tags[j]['_pivot_tag_id'];
+				}
+			}
+			res.json(result)
 		})
 		.catch(function (err) {
-			console.log(err)
       res.status(500).json({error: {message: err.message}});
     });
 	});
