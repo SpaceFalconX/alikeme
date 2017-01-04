d18d8bdb1a604141848aaf3b0a7dcc68baa3d2b6
astelvida <sevda.m.anefi@gmail.com>
2017-01-03 18:09:54 -0800

    pull changes in

--- server/routes/post_router.js
+++ server/routes/post_router.js
@@ -21,29 +21,19 @@ router.route('/posts')
 				const allTagsNames = allTags.pluck('name');
 				const newTagsNames = JSON.parse(tags)
 				const tagsToSave = _.difference(newTagsNames, allTagsNames);
-				const promisedTags = tagsToSave.map((tagName) => {
-					return new Tag({name: tagName}).posts().attach(post);
+				const promisedTags = tagsToSave.map((tagName, index) => {
+					return Tag.forge({name: tagName}).save();
 				})
-				Promise.all(promisedTags)
-				.then((result) => {
-					console.log("RESULT", result)
+				return Promise.all(promisedTags)
+				.then((results) => {
+					post.tags().attach(results);
+					console.log("TAGS, POSTS and POSTS_TAGS have been updated")
+					res.json({data: "SUCCESS: created post and added tags" })
 				})
 			})
 		})
 	})
-
-// var admin1 = new Admin({username: 'user1', password: 'test'});
-// var admin2 = new Admin({username: 'user2', password: 'test'});
-
-// Promise.all([admin1.save(), admin2.save()])
-//   .then(function() {
-//     return Promise.all([
-//     new Site({id: 1}).admins().attach([admin1, admin2]),
-//     new Site({id: 2}).admins().attach(admin2)
-//   ]);
-// })
-
-
+	
 	.get((req, res) => {
 		Posts.forge()
 		.fetch({withRelated: ['tags', 'category']})
