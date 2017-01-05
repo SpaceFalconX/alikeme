f99b0ba52893394df0d72fd4971904c19330fae2
Wasiff <wasiff@gmail.com>
2017-01-04 17:53:27 -0800

    build Watson API

--- server/config/twitter.js
+++ server/config/twitter.js
@@ -1,6 +1,29 @@
-module.exports = {
+const twitter = {
   consumer_key:         'PbfgGoSHba3DWxY4kpT8Lztu1',
   consumer_secret:      'F6wVQ3xIfZXngTcZwvfT40HMZ4Q8K8RgdB3Q2YhuBwJJUqVevt',
   access_token:         '812385770484822016-YYML4Fa7afJT7tt7lgCv6j6hx88W52R',
   access_token_secret:  '6LtmeVQ9qdVPeFR19kBun1uGXbwcPJVW9eszpwmameCX5'
 }
+const _ = require('underscore');
+const Twit  = require('twit');
+const T = new Twit(twitter);
+const options = { 
+	screen_name: 'hackreactor',
+	include_rts: false,
+  count: 100 
+}
+
+const getTwitterFeed = () => {
+   return new Promise ((res, rej)=>{
+    T.get('statuses/user_timeline', options, (err, feed) => {
+			if (err) { 
+        rej(err);
+      } else {
+        const twitterFeed = _.pluck(feed, 'text').join()
+        res(twitterFeed);
+      }
+		})
+   })
+ }
+
+module.exports = getTwitterFeed;
---
+++ server/routes/watson_router.js
@@ -0,0 +1,35 @@
+const getTwitterFeed = require('../config/twitter.js')
+const personality_insights = require('../config/ibm_watson.js');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/personality', (req, res) => {
+  getTwitterFeed().then((feed)=> {
+    let traits = {}
+    personality_insights.profile({ text: feed }, (err, result) => {
+        if (err) { 
+          res.status(400).send({error: err.message})
+        }
+        res.status(200).send(result)
+        //   traits.Openness = response.tree.children[0].children[0].children[0].percentage
+        //   traits.Conscientiousness = response.tree.children[0].children[0].children[1].percentage
+        //   traits.Extraversion = response.tree.children[0].children[0].children[2].percentage
+        //   traits.Agreeableness = response.tree.children[0].children[0].children[3].percentage
+        //   traits['Emotional range'] = response.tree.children[0].children[0].children[4].percentage
+        //   /** CHECKING CONSOLE */
+        //   console.log(`***IBM WATSON***`)
+        // //console.log(JSON.stringify(response, null, 2))
+        //   console.log(`Openness, ${response.tree.children[0].children[0].children[0].percentage}`)
+        //   console.log(`Conscientiousness, ${response.tree.children[0].children[0].children[1].percentage}`)
+        //   console.log(`Extraversion', ${response.tree.children[0].children[0].children[2].percentage}`)
+        //   console.log(`Agreeableness', ${response.tree.children[0].children[0].children[3].percentage}`)
+        //   console.log(`Emotional range', ${response.tree.children[0].children[0].children[4].percentage}`)
+    })
+  })
+  .catch((err) => {
+    res.status(404).send({error: err.message})
+  })
+})
+
+module.exports = router
