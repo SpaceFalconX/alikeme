a18a3b60eebbf6ea89e55f50e56afc41945ce721
Isaac <Isaac@Isaacs-MacBook-Pro-2.local>
2017-01-08 00:23:26 -0800

    small fix

--- client/app/components/Browse.jsx
+++ client/app/components/Browse.jsx
@@ -6,11 +6,11 @@ import { fetchPostsFromDb, filterPostsFromDb} from '../actions/post_actions.js'
 
 
 class Browse extends React.Component {
-  constructor(props) {
+  constructor (props) {
     super()
-    // this.state = {
-    //   searchTerm : ''
-    // }
+    this.state = {
+      filter: null
+    }
   }
 
   filter (e) {
@@ -18,22 +18,37 @@ class Browse extends React.Component {
     let search = this.props.categories.filter((category) => {
       return this.refs.search.value === category.name
     })[0]
-    console.log(search.id)
+    //todo - if search.length === 0 then dispatch a different filter to filter by tag instead
     this.props.dispatch(filterPostsFromDb(search.id))
+    this.setState({filter: this.refs.search.value})
     this.refs.search.value = "";
   }
 
   componentWillMount() {
-    // if(this.props.allPosts.length === 0){
-    //   this.props.dispatch(fetchPostsFromDb())
-    // }
+    if(this.props.allPosts.length === 0){
+      this.props.dispatch(fetchPostsFromDb())
+    }
     if(this.props.categories.length === 0) {
       this.props.dispatch(fetchCategories());
     }
   }
 
+  clearFilter () {
+    this.setState({filter: null})
+    this.props.dispatch(fetchPostsFromDb())
+  }
+
+  filterTags () {
+    if(!this.state.filter){
+      return
+    }
+    return (
+      <div onClick={this.clearFilter.bind(this)}>FILTERING BY {this.state.filter.toUpperCase()} - CLICK TO CLEAR</div>
+    )
+
+  }
+
   render () {
-    console.log(this.props)
     let sorted = this.props.allPosts.sort((a,b) => {
       return a.id < b.id
     })
@@ -47,7 +62,10 @@ class Browse extends React.Component {
               <button>search</button>
               </form>
             </div>
-            <div className="list-group">
+
+            {this.filterTags()}
+            <div className='container'>
+
               { sorted.map((post) => {
                   return (
                     <Post key={post.id} post={post} contextUser={this.props.user.username} />
