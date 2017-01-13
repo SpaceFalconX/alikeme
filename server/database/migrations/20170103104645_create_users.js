34782930d4e88db975cf2b104192b991b8bce6a3
astelvida <sevda.m.anefi@gmail.com>
2017-01-12 21:08:03 -0800

    added location and timestamps defaults to user table

--- server/database/migrations/20170103104645_create_users.js
+++ server/database/migrations/20170103104645_create_users.js
@@ -5,6 +5,7 @@ exports.up = (knex, Promise) => {
     user.string('username', 254).unique().notNullable();
     user.string('email', 254).unique().notNullable();
     user.string('password', 254).notNullable();
+    user.string('city', 254).defaultTo('San Francisco');
     user.string('twitterLink', 254);
     user.string('facebookLink', 254);
     user.float('openness').unsigned();
@@ -12,7 +13,8 @@ exports.up = (knex, Promise) => {
     user.float('extraversion').unsigned();
     user.float('agreeableness').unsigned();
     user.float('emotionalRange').unsigned();
-    user.timestamps();
+    user.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+    user.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
   })
   .then(() => {
   	console.log('USERS table created!')
--- server/routes/upload_router.js
+++ server/routes/upload_router.js
@@ -11,21 +11,21 @@ const storage = multer.diskStorage({
 const upload = multer({ storage }).any();
 
 const cloudinary = require('cloudinary');
-cloudinary.config({ 
-  cloud_name: 'isaacxpreston', 
-  api_key: '297943853264417', 
-  api_secret: 'LqbCuQRKxRksxHiqdf_M5GO2oZk' 
+cloudinary.config({
+  cloud_name: 'isaacxpreston',
+  api_key: '297943853264417',
+  api_secret: 'LqbCuQRKxRksxHiqdf_M5GO2oZk'
 })
 
 let username;
 
-router.post('/setUserName', (req, res) => {
+router.post('/setUserNamesetUserName', (req, res) => {
   username = req.body.username
   res.sendStatus(200)
 })
 
 router.post('/uploadProfilePicture', upload, (req, res) => {
-  cloudinary.v2.uploader.upload('profile_pictures/' + username + '.jpg', {public_id: username, invalidate: true}, function(err, result) { 
+  cloudinary.v2.uploader.upload('profile_pictures/' + username + '.jpg', {public_id: username, invalidate: true}, function(err, result) {
     //console.log("it broke?", err, result)
     var testImg = cloudinary.image(username + '.jpg')
     res.send(testImg)
