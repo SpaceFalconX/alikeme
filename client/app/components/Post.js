63539b5f4a1d433234437d775dfc4e320b75298f
Wasiff <wasiff@gmail.com>
2017-01-13 20:29:09 -0800

    update browse

--- client/app/components/Post.js
+++ client/app/components/Post.js
@@ -3,33 +3,29 @@ import {connect} from 'react-redux';
 import {browserHistory, Link} from 'react-router';
 import moment from 'moment'
 import axios from 'axios'
-import StarButton from './StarButton.js'
 import UserPic from './userPicture.js'
 
 class Post extends React.Component {
-
+  
   postStyle () {
     return {margin: '0px 3px 0px 3px',}
   }
 
   renderTags () {
     return this.props.post.tags.map((tag, index) => {
-      if(tag.name) {
-        tag = tag.name
-      }
-      return (<Link key={index}><span className="label label-info" style={this.postStyle()}>{tag}</span></Link>)
+      return (<Link key={index}><span className="label label-info" style={this.postStyle()}>{tag.name}</span></Link>)
     })
   }
 
   usernameContext () {
     if(this.props.post.username) {
-      return this.props.post.username  //what is this user.username and post.username
+      return this.props.post.username
     }
     return this.props.post.user.username
   }
 
   matchORViewContext () {
-    if(this.props.contextUser && this.props.contextUser !== this.usernameContext()) { //what is contextUser
+    if(this.props.contextUser && this.props.contextUser !== this.usernameContext()) { 
       return (
         <Link to={'/profile/' + this.usernameContext()}> click to view {this.usernameContext()}'s profile</Link>
       )
@@ -40,8 +36,13 @@ class Post extends React.Component {
   }
 
   render () {
+    const POST_CSS = {
+      boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)',
+      transition: '0.3s'
+    }
+
     return (
-        <div className="panel panel-default">
+        <div className="panel panel-default" style={POST_CSS}>
           <div className="panel-body">
             <Link className="pull-left">
               <UserPic username={this.usernameContext()} />
@@ -56,7 +57,6 @@ class Post extends React.Component {
               <p>{this.matchORViewContext()}</p>
             </div>
           </div>
-          <div>{this.props.post.stars_count}</div>
           <div className="panel-body">
             <span className="glyphicon glyphicon-tags" aria-hidden="true" style={this.postStyle()}></span>
             <span>{this.renderTags()}</span>
