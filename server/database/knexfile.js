3b73264654e51fc51ffe217b18a23b64ce5ed00c
astelvida <sevda.m.anefi@gmail.com>
2017-01-15 21:52:53 -0800

    Use heroku ClearDB

--- server/database/knexfile.js
+++ server/database/knexfile.js
@@ -1,12 +1,21 @@
 // Update with your config settings.
 module.exports = {
+  // development: {
+  //   client: 'mysql',
+  //   connection: {
+  //     host     : 'localhost',
+  //     user     : 'root',
+  //     password : 'io',
+  //     database : 'alike_me',
+  //     charset  : 'utf8'
+  //   }
   development: {
     client: 'mysql',
     connection: {
-      host     : 'localhost',
-      user     : 'root',
-      password : 'io',
-      database : 'alike_me',
+      host     : 'us-cdbr-iron-east-04.cleardb.net',
+      user     : 'b2c1e11015ee11',
+      password : '90bb8fcd',
+      database : 'heroku_e49323e7b0a627f',
       charset  : 'utf8'
     }
     // debug : true
