5c6b2bb6bb203c65e7fe9fbf6e9df02c215e3374
astelvida <sevda.m.anefi@gmail.com>
2017-03-02 22:18:12 -0800

    add db

--- server/database/knexfile.js
+++ server/database/knexfile.js
@@ -1,40 +1,29 @@
 // Update with your config settings.
 module.exports = {
-  development: {
-    client: 'mysql',
-    connection: {
-      host     : 'localhost',
-      user     : 'root',
-      password : 'io',
-      database : 'alike_me',
-      charset  : 'utf8'
-    }
-  }
   // development: {
   //   client: 'mysql',
   //   connection: {
-  //     host     : 'us-cdbr-iron-east-04.cleardb.net',
-  //     user     : 'b2c1e11015ee11',
-  //     password : '90bb8fcd',
-  //     database : 'heroku_e49323e7b0a627f',
+  //     host     : 'localhost',
+  //     user     : 'root',
+  //     password : 'io',
+  //     database : 'alike_me',
   //     charset  : 'utf8'
   //   }
-  //   // debug: true
   // }
-  // development: {
-  //     client: 'mysql',
-  //     connection: {
-  //       host     : 'us-cdbr-iron-east-04.cleardb.net',
-  //       user     : 'b2c1e11015ee11',
-  //       password : '90bb8fcd',
-  //       database : 'heroku_e49323e7b0a627f',
-  //       charset  : 'utf8'
-  //     },
+  development: {
+      client: 'mysql',
+      connection: {
+        host     : 'us-cdbr-iron-east-04.cleardb.net',
+        user     : 'b2c1e11015ee11',
+        password : '90bb8fcd',
+        database : 'heroku_e49323e7b0a627f',
+        charset  : 'utf8'
+      },
 
-  //     migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+      migrations: {
+        tableName: 'knex_migrations'
+    }
+  }
 
 }
 
