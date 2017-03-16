df4d7504e55ad46975d639a4194c04e5391e5d63
astelvida <sevda.m.anefi@gmail.com>
2017-03-15 17:25:44 -0700

    re-enabled IBM Watson tokens

--- server/config/ibm_watson.js
+++ server/config/ibm_watson.js
@@ -3,12 +3,8 @@
 const PersonalityInsightsV2 = require('watson-developer-cloud/personality-insights/v2')
 
 const personality_insights = new PersonalityInsightsV2({
-  username: 'b1458fd6-2abc-4074-8bb6-',
+  username: 'b1458fd6-2abc-4074-8bb6-1c1b89ce3e29',
   password: 'nXQpgyjzwyIl'
 });
-// const personality_insights = new PersonalityInsightsV2({
-//   username: 'b1458fd6-2abc-4074-8bb6-1c1b89ce3e29',
-//   password: 'nXQpgyjzwyIl'
-// });
 
 module.exports = personality_insights;
